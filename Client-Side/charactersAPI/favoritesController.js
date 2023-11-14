ec7c67a33cbe081b5e9893784979b1d7507c0333
hayesyoung <hy00423@GeorgiaSouthern.edu>
2023-11-14 10:01:29 -0500

    Forgot to save this one

--- Client-Side/charactersAPI/favoritesController.js
+++ Client-Side/charactersAPI/favoritesController.js
@@ -1,22 +1,30 @@
 const fs = require('fs');
 const path = require("path");
 
-class FavoritesAPI {
+class favoritesAPI {
     constructor() {
         this.filePath = path.resolve('./data/favoritesData.json');
         this.favoritesData = JSON.parse(fs.readFileSync(this.filePath));
     }
 
-    addFavorite(characterId) {
-        if (!this.favoritesData.includes(characterId)) {
-            this.favoritesData.push(characterId);
+    addFavorite(id) {
+        if (!this.favoritesData.includes(id)) {
+            this.favoritesData.push(id);
             fs.writeFileSync(this.filePath, JSON.stringify(this.favoritesData));
         }
     }
 
+    removeFavorite(id) {
+        const index = this.favoritesData.indexOf(id);
+        if (index > -1) {
+            this.favoritesData.splice(index, 1);
+            fs.writeFileSync(this.filePath, JSON.stringify(this.favoritesData));
+        }
+    }
+    
     getFavorites() {
         return this.favoritesData;
     }
 }
 
-module.exports = FavoritesAPI;
+module.exports = favoritesAPI;
