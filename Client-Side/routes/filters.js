ec2ff93598ecb882f94c55df4be5866e62fd555c
LadyWorldTown <103071764+LadyWorldTown@users.noreply.github.com>
2023-11-16 11:58:47 -0500

    Update filters.js

--- Client-Side/routes/filters.js
+++ Client-Side/routes/filters.js
@@ -6,7 +6,11 @@ var filtersController = require('../filtersAPI/filtersController');
 
 //put routes in here
 
+router.get('/', function(req, res, next) {
+    let filter = new filtersController();
+    filter.getMember();
 
+});
 
 
 
