a9c3497b75a66da65b4acd9eaac15680a9d960da
LadyWorldTown <103071764+LadyWorldTown@users.noreply.github.com>
2023-11-28 18:59:14 -0500

    Updated Filters

--- Client-Side/routes/filters.js
+++ Client-Side/routes/filters.js
@@ -4,22 +4,34 @@ var router = express.Router();
 
 var filtersController = require('../filtersAPI/filtersController');
 
-router.get('/', function(req, res, next){
+router.get('/', function (req, res, next) {
     controller = new filtersController();
     result = controller.getFilters();
     res.send(result);
 });
 
-router.get('/byUniverse/:universe', function(req, res, next){
+router.get('/byUniverse/:universe', function (req, res, next) {
     controller = new filtersController();
     result = controller.getFiltersByUniverse(req.params.universe);
     res.send(result);
 });
 
-router.get('/byFavorite/:favorite', function(req, res, next){
+router.get('/byFavorite/:favorite', function (req, res, next) {
     controller = new filtersController();
     result = controller.getFiltersByFavorite(req.params.favorite);
     res.send(result);
 });
+router.patch('/update/:id/:status', function (req, res, next) {
+
+    controller = new filtersController();
+    var id = parseInt(req.params.id);
+    var newFavoriteStatus = (req.params.status === 'true') ? true : false;
+    result = controller.updateFavoriteStatus(id, newFavoriteStatus);
+
+    res.send(result);
+});
+
+
+
 
 module.exports = router;
